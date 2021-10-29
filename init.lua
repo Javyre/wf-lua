3cc53444a13107935604a3d69430cee1b89c0d0b
Javier A. Pollak <javi.po.123@gmail.com>
2021-10-28 20:38:19 -0400

    feat(view): Add hook/unhook methods to views
    It's now possible to listen for signals on a specific view.

--- init.lua
+++ init.lua
@@ -2,24 +2,24 @@ local wf = require('wf')
 
 print('Hello, lua!')
 
-wf.set{
+wf.set {
     'zoom',
-    interpolation_method = 0, -- Linear interpolation
+    interpolation_method = 0 -- Linear interpolation
 }
 
 local function print_output(output)
-    print('output:          '..tostring(output))
-    print('output workarea: '..tostring(output:get_workarea()))
+    print('output:          ' .. tostring(output))
+    print('output workarea: ' .. tostring(output:get_workarea()))
 end
 
 local function print_view(view)
-    print('view:                 '..tostring(view))
-    print('view title:           '..tostring(view:get_title()))
-    print('view app_id:          '..tostring(view:get_app_id()))
-    print('view wm_geometry:     '..tostring(view:get_wm_geometry()))
-    print('view output_geometry: '..tostring(view:get_output_geometry()))
-    print('view bounding_box:    '..tostring(view:get_bounding_box()))
-    print('view output:          '..tostring(view:get_output()))
+    print('view:                 ' .. tostring(view))
+    print('view title:           ' .. tostring(view:get_title()))
+    print('view app_id:          ' .. tostring(view:get_app_id()))
+    print('view wm_geometry:     ' .. tostring(view:get_wm_geometry()))
+    print('view output_geometry: ' .. tostring(view:get_output_geometry()))
+    print('view bounding_box:    ' .. tostring(view:get_bounding_box()))
+    print('view output:          ' .. tostring(view:get_output()))
 end
 
 local handler
@@ -29,10 +29,15 @@ handler = wf.outputs:hook('view-mapped', function(output, data)
     print_view(data.view)
 
     if data.view:get_app_id() == 'foot' then
-        data.view:set_geometry({1, 2, 300, 500})
-    end
+        data.view:set_geometry({1, 2, 500, 300})
 
-    -- wf.outputs:unhook('view-mapped', handler)
+        data.view:hook('title-changed', function(view, data)
+            print('>>> View title changed! New title:', data.view:get_title())
+            assert(view == data.view)
+        end)
+
+        wf.outputs:unhook('view-mapped', handler)
+    end
 end)
 
 wf.outputs:hook('view-unmapped', function(output, data)
