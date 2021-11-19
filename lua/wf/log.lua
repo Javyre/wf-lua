816f92e0468f74efcf7e7c23aef0d66a6aa42cc1
Javier A. Pollak <javi.po.123@gmail.com>
2021-11-19 14:17:55 -0500

    log: Add log_level setting

--- lua/wf/log.lua
+++ lua/wf/log.lua
@@ -1,18 +1,24 @@
-local Log = {}
+local Log = {
+    log_level = 0
+}
 
 local function to_strings(list)
     for i, a in ipairs(list) do list[i] = tostring(a) end
     return list
 end
 
-local function log(color, ...)
+local function log(lvl, color, ...)
+    if Log.log_level < lvl then
+        return
+    end
+
     io.stderr:write('[' .. color .. 'm')
     io.stderr:write(unpack(to_strings({...})))
     io.stderr:write('[m\n')
 end
 
-function Log.err(...) return log(31, 'EE: ', ...) end
-function Log.warn(...) return log(33, 'WW: ', ...) end
-function Log.debug(...) return log(0, 'DD: ', ...) end
+function Log.err(...) return log(0, 31, 'EE: ', ...) end
+function Log.warn(...) return log(1, 33, 'WW: ', ...) end
+function Log.debug(...) return log(2, 0, 'DD: ', ...) end
 
 return Log
